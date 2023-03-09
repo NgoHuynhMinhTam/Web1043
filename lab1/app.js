fb75ba663ec66d125e0f281a825f7bba6e5ff253
tamminh <tamnhmpd07407@fpt.edu.vn>
2023-03-09 23:46:18 +0700

    message

---
+++ lab1/app.js
@@ -0,0 +1,12 @@
+//javascrift for lab 1
+//khai bao bien a va nhan gia tri tu ban phim
+let a = prompt("Moi ban nhap so nguyen a:");
+let ai = parseInt(a);
+let b = prompt("Moi ban nhap so nguyen b:");
+let bi = parseInt(b);
+//let c = ai + bi;
+let c = parseInt(a) + parseInt(b);
+let x = a + b;
+document.write(c);
+console.log(c);
+//document.write(x);
